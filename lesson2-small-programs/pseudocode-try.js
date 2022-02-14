cf5ce547f6a439983ddce6f3ee3ab58c93beb135
Daevah Taylaran <daevtayl@gmail.com>
2022-02-13 16:08:43 -0800

    pseudocode fun

--- lesson2-small-programs/pseudocode-try.js
+++ lesson2-small-programs/pseudocode-try.js
@@ -39,4 +39,151 @@ Write pseudocode that attempts to solve:
   merge([1, 2, 3], [4, 5, 6]); // => [1, 4, 2, 5, 3, 6]
 
 ______________________________________________________________________________ 
+
+1. Function that takes two arguments and returns the sum of its arguments
+
+START
+SET function sum(num1, num2) {
+  READ num1 and num2
+  PRINT/RETURN num1 + num2
+}
+END
 */
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(sum(4, 5)); // 9
+
+/*
+______________________________________________________________________________ 
+
+2. Function that returns the input (array of strings) concatenated together
+
+// concatenate all of the elements in an array as one new string
+
+START
+SET function combine(array) {
+  PRINT/RETURN array.concat('');
+}
+END
+*/
+
+function combine(array) {
+  return array.join('');
+}
+
+let friends = ['Monica', 'Phoebe', 'Rachel', 'Chandler', 'Joey', 'Ross'];
+let animal = ['d', 'o', 'g'];
+console.log(combine(friends));
+console.log(combine(animal));
+
+/*
+______________________________________________________________________________ 
+
+3. method that takes an array of integers, and returns a new array
+  with [every other element] from the original array, 
+  starting with the first element. 
+
+  //function that returns the elements that have the even index values 
+  from the original array
+
+  START
+  SET function evenIndex(array) {
+    SET newArray = empty array
+    WHILE iterator < array.length
+      IF iterator is even
+        GET array with iterator index 
+        push to newArray
+    PRINT/SAVE newArray
+  }
+  END
+*/
+
+function evenIndex(array) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (i % 2 === 0) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
+
+let numArray = [4, 2, 3, 6, 22, 11, 12, 5];
+console.log(evenIndex(numArray)); // [4, 3, 22, 12]
+
+/*
+______________________________________________________________________________ 
+
+4. function that determines the index of the 3rd 
+  occurrence of a given character in a string. 
+  For instance, if the given character is 'x' 
+  and the string is 'axbxcdxe[x]', the function 
+  should return 6 (the index of the 3rd 'x'). 
+  If the given character does not occur at least 3 times, return null.
+
+  START 
+  SET function thirdIndex(string, character) {
+    SET counter is equal to 0;
+    WHILE iterator < string length
+      if string with the iterator as an index is equal to character
+      add to counter 
+      IF counter is equal to 3
+        PRINT/RETURN : the index of the 3rd character is: index
+    PRINT/RETURN : null (defaults if index not found)
+        
+  }
+  END
+*/
+
+function thirdIndex(string, char) {
+  let counter = 0; 
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === char) {
+      counter ++;
+    }
+    if (counter === 3) {
+      return `The index of the 3rd "${char}" is ${i}`;
+    }
+  }
+  return null;
+}
+
+console.log(thirdIndex('abcabcab', 'a'));
+
+/*
+______________________________________________________________________________ 
+
+5. function that takes two arrays of numbers and returns 
+the result of merging the arrays. 
+
+The elements of the first array should become 
+the elements at the even indexes of the returned array, 
+
+while the elements of the second array should 
+become the elements at the odd indexes
+
+can assume that both arrays have the same length
+
+  START
+  SET function mergeArray(firstArray, secArray) {
+    SET newArray to an empty array
+    WHILE iterator is less than newArray.length
+      push firstArray with iterator element to newArray
+      push secondArray with iterator element to newArry
+    PRINT/RETURN mergeArray
+  }
+  END
+*/
+
+function mergeArray(firstA, secA) {
+  let newA = [];
+  for (let i = 0; i < firstA.length; i++) {
+    newA.push(firstA[i]);
+    newA.push(secA[i]);
+  }
+  return newA
+}//end
+
+console.log(mergeArray([1, 2, 3], [4, 5, 6])); //[1, 4, 2, 5, 3, 6]
